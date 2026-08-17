1df87c7efbc6a430df8219dd3900a7bf32c3cdf2
UmakraftCircle <pagaranjayson04@protonmail.com>
2026-08-16 21:48:06 -0700

    feat(memory): add /ask command + handlers + aiService wiring

--- packages/integrations/src/index.ts
+++ packages/integrations/src/index.ts
@@ -130,3 +130,4 @@ export const allIntegrations = [discordSendMessage, databaseStoreResult];
 export * from './database.js';
 export * from './turso.js';
 export * from './trainer-links.js';
+export * from './conversation-memory.js';
